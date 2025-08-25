90c6a8c8c67c0e57300bc11b527ded9a873c6f21
Archana MohanDas <archanamdasofficial@gmail.com>
2025-08-25 23:49:52 +0530

    Third

--- src/Home.js
+++ src/Home.js
@@ -18,7 +18,7 @@ export default function Home () {
             loadData()
             
         }catch (error) {
-            console.error("failed to fetch data")
+            alert("failed to fetch data")
         }
         
     },[])
